04b5c88a306a3d5e60a7b242961ad3d3511b0cb5
unknown <dy.yev.solovyov@gmail.com>
2023-10-05 09:43:55 +0300

    fix layout p.2

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,21 +1,29 @@
-import Layout from "@/layouts/Layout";
-import SectionLayout from "@/layouts/SectionLayout";
+import { classNames } from "@/utils";
+
+import Layout from "@/components/layouts/Layout";
 
 const Home = () => {
-	const sections = [
-		{ id: 1, link: "ABOUT" },
-		{ id: 2, link: "SKILLS" },
-		{ id: 3, link: "PROJECTS" },
-		{ id: 4, link: "HIRE" },
-	];
-
 	return (
 		<Layout>
-			{sections.map(({ id, link }) => (
-				<SectionLayout key={id} id={id} link={link}>
-					{link}
-				</SectionLayout>
-			))}
+			<section id="about" className={classNames("min-h-screen flex justify-center px-2 bg-gray-400")}>
+				<div className={classNames("w-full sm:w-[90%] md:w-[75%] lg:w-[60%] border border-red-500")}>ABOUT</div>
+			</section>
+
+			<section id="skills" className={classNames("min-h-screen flex justify-center px-2 bg-gray-500")}>
+				<div className={classNames("w-full sm:w-[90%] md:w-[75%] lg:w-[60%] border border-red-500")}>
+					SKILLS
+				</div>
+			</section>
+
+			<section id="projects" className={classNames("min-h-screen flex justify-center px-2 bg-gray-600")}>
+				<div className={classNames("w-full sm:w-[90%] md:w-[75%] lg:w-[60%] border border-red-500")}>
+					PROJECTS
+				</div>
+			</section>
+
+			<section id="hire" className={classNames("min-h-screen flex justify-center px-2 bg-gray-700")}>
+				<div className={classNames("w-full sm:w-[90%] md:w-[75%] lg:w-[60%] border border-red-500")}>HIRE</div>
+			</section>
 		</Layout>
 	);
 };
